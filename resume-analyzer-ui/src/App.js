63e82ff0a6c994680159dedcfa43b6d0cae8d5b5
ButtSmasher <patelchirang10@gmail.com>
2025-08-03 14:50:10 +0530

    changes for deployment

--- resume-analyzer-ui/src/App.js
+++ resume-analyzer-ui/src/App.js
@@ -54,7 +54,7 @@ function App() {
     formData.append("job_description", jobDescription);
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/analyze-resume/", {
+      const response = await fetch("https://resumeanalyzer-backend-ocw2.onrender.com/analyze-resume/", {
         method: "POST",
         body: formData,
       });
